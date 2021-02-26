35071c94005e6302ce3b6ed4af2c3f5ef6c6b173
hafizkalam <hafizkalamabdillah@gmail.com>
2021-02-26 11:35:18 +0700

    cms

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,5 @@ use Illuminate\Contracts\Container\BindingResolutionException;
 
 
 Route::get('/', function () {
-    echo "Selamat Datang";
-});
-
-Route::get('/about', function () {
-    echo "1941720233 , Nazario Safariesqi Tyo Widjaya , TI2A";
-});
-
-Route::get('/articles/{$id}', function ($id) {
-    return "Halaman Artikel dengan ID ".$id;
-});
-
-Route::get('/', 'HomeController@index');
-Route::get('/about', 'AboutController@about');
-Route::get('/articles', 'ArticlesController@articles');
+    return view('index', ['name' => '']);
+    });
